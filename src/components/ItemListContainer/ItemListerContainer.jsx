742b979d07d295d37852914a91df953fefe84434
Diego Delgado <diegodelgado365@gmail.com>
2024-01-14 12:03:10 -0300

    first commit

--- src/components/ItemListContainer/ItemListerContainer.jsx
+++ src/components/ItemListContainer/ItemListerContainer.jsx
@@ -1,5 +1,5 @@
 import { Products } from "../Products/Products";
-import "./itemListContainer.css";
+import "./ItemListContainer.css";
 import { useParams } from "react-router-dom";
 
 //Mostramos los items por categoría. En caso en que no se encuentre la categoría muestra todos los items
